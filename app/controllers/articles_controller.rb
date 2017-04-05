d8a7ac93ae491b7ce5251d1158ce7f1b9ff13fe0
Robert Jan Hogerbrugge <rj.hogerbrugge@hotmail.com>
2017-04-05 17:41:55 +0200

    Article controller and form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,34 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :destroy, :update]
+
   def index
     @articles = Article.all.paginate(:page => params[:page], :per_page => 2)
   end
 
   def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: "Article saved!"
+    else
+      render :new, notice: "Something went wrong. Article was not saved."
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
