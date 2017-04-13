b5f478d39fc29d2cb60ecafbaa3d8f3383308234
Robert Jan Hogerbrugge <rj.hogerbrugge@hotmail.com>
2017-04-13 10:56:23 +0200

    model tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  RSpec.describe Article, type: :model do
+    before(:all)do
+      @article = Article.new(content:"My Article", title:"My Title")
+    end
+    it "should have matching content" do
+      expect(@article.content).to eq("My Article")
+    end
+    it "should have a matching title" do
+      expect(@article.title).to eq("My Title")
+    end
+  end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  RSpec.describe Project, type: :model do
+    before(:all)do
+      @project = Project.new(description:"My Project", title:"My Title", link:"www.mylink.nl")
+    end
+    it "should have matching description" do
+      expect(@project.description).to eq("My Project")
+    end
+    it "should have a matching title" do
+      expect(@project.title).to eq("My Title")
+    end
+    it "should have a matching link" do
+      expect(@project.link).to eq("www.mylink.nl")
+    end
+  end
